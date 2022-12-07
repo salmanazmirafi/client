5ae5befd1b26a8f9611f527b3da95f03c62c85b4
salmanazmirafi <116106498+salmanazmirafi@users.noreply.github.com>
2022-12-07 22:08:28 +0600

    Update

--- src/component/featued/Featude.jsx
+++ src/component/featued/Featude.jsx
@@ -3,55 +3,49 @@ import useFetch from "../../hooks/useFetch";
 import "./featude.css";
 
 const Featude = () => {
-  const { data, loding } = useFetch(
-    "/hotels/countByCity?cities=new,london,berline"
-  );
+  const { data } = useFetch("/hotels/countByCity?cities=new,london,berline");
 
   return (
     <div className="featured">
-      {loding ? (
-        "Loading......."
-      ) : (
-        <>
-          {data && (
-            <>
-              <div className="featuredItem">
-                <img
-                  src="https://cdn.pixabay.com/photo/2021/06/01/12/39/beach-6301597__340.jpg"
-                  alt=""
-                  className="featuredImg"
-                />
-                <div className="featuredTitles">
-                  <h1>New</h1>
-                  <h2>{data[0]} properties</h2>
-                </div>
+      <>
+        {data && (
+          <>
+            <div className="featuredItem">
+              <img
+                src="https://cdn.pixabay.com/photo/2021/06/01/12/39/beach-6301597__340.jpg"
+                alt=""
+                className="featuredImg"
+              />
+              <div className="featuredTitles">
+                <h1>New</h1>
+                <h2>{data[0]} properties</h2>
               </div>
-              <div className="featuredItem">
-                <img
-                  src="https://cdn.pixabay.com/photo/2019/10/08/04/16/dragon-palace-hotel-4534092__340.jpg"
-                  alt=""
-                  className="featuredImg"
-                />
-                <div className="featuredTitles">
-                  <h1>London</h1>
-                  <h2>{data[1]} properties</h2>
-                </div>
+            </div>
+            <div className="featuredItem">
+              <img
+                src="https://cdn.pixabay.com/photo/2019/10/08/04/16/dragon-palace-hotel-4534092__340.jpg"
+                alt=""
+                className="featuredImg"
+              />
+              <div className="featuredTitles">
+                <h1>London</h1>
+                <h2>{data[1]} properties</h2>
               </div>
-              <div className="featuredItem">
-                <img
-                  src="https://cdn.pixabay.com/photo/2013/07/19/00/18/water-165219__340.jpg"
-                  alt=""
-                  className="featuredImg"
-                />
-                <div className="featuredTitles">
-                  <h1>Berline</h1>
-                  <h2>{data[2]} properties</h2>
-                </div>
+            </div>
+            <div className="featuredItem">
+              <img
+                src="https://cdn.pixabay.com/photo/2013/07/19/00/18/water-165219__340.jpg"
+                alt=""
+                className="featuredImg"
+              />
+              <div className="featuredTitles">
+                <h1>Berline</h1>
+                <h2>{data[2]} properties</h2>
               </div>
-            </>
-          )}
-        </>
-      )}
+            </div>
+          </>
+        )}
+      </>
     </div>
   );
 };
